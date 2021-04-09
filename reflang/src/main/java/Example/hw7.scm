caae4fbffac6720cd7968960485e78988a1a690d
enzoca <enzoca@iastate.edu>
2021-04-09 12:47:00 -0500

    Outlined functions for P1

--- reflang/src/main/java/Example/hw7.scm
+++ reflang/src/main/java/Example/hw7.scm
@@ -1,3 +1,17 @@
+
+
+
+//Homework-provided code
+//Implementation of a linked list
+(define pairNode (lambda (fst snd) (lambda (op) (if op fst snd))))
+
+(define node (lambda (x) (pairNode x (ref (list)))))
+
+(define getFst (lambda (p) (p #t)))
+
+(define getSnd (lambda (p) (p #f)))
+
+//Implementation of a binary tree
 (define treeNode (lambda () ()))
 
 (define node (lambda (num) ()))
