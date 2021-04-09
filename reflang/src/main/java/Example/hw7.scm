761e5d70f4f864c5b863809cf0cbf4a1759f6c8c
enzoca@iastate.edu <Homestuck-Sucks!>
2021-04-09 15:26:34 -0500

    First pass at proper binary tree

--- reflang/src/main/java/Example/hw7.scm
+++ reflang/src/main/java/Example/hw7.scm
@@ -1,23 +1,35 @@
 
+(define treeNode
+    (lambda (val left right)
+        (lambda (op1 op2)
+            (if op1 val
+                (if op2 left right)
+            )
+        )
+    )
+)
 
+(define node
+    (lambda (x)
+        (treeNode x (ref (list)) (ref (list))
+        )
+    )
+)
 
-//Homework-provided code
-//Implementation of a linked list
-(define pairNode (lambda (fst snd) (lambda (op) (if op fst snd))))
+(define value
+    (lambda (node)
+        (node #t #t)
+    )
+)
 
-(define node (lambda (x) (pairNode x (ref (list)))))
+(define left
+    (lambda (node)
+        (node #f #t)
+    )
+)
 
-(define getFst (lambda (p) (p #t)))
-
-(define getSnd (lambda (p) (p #f)))
-
-//Implementation of a binary tree
-(define treeNode (lambda () ()))
-
-(define node (lambda (num) ()))
-
-(define value (lambda (node) ()))
-
-(define left (lambda (node) ()))
-
-(define right (lambda (node) ()))
+(define right
+    (lambda (node)
+        (node #f #f)
+    )
+)
